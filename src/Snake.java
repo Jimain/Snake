ff51c869648a30b3e1653b27e029a2b839376d50
KIS <KIS@1506009.local>
2017-11-08 19:50:09 +0900

    Add JFrame

---
+++ src/Snake.java
@@ -0,0 +1,36 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class Snake {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame();
+		
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		int dH = (int) dimension.getHeight();
+		int dW = (int) dimension.getWidth();
+		
+		int frameHeight = dH / 2;
+		int frameWidth = dW / 3;
+		
+		int xp = ( dW / 2 ) -  ( frameWidth / 2 );
+		int yp = ( dH / 2 ) - ( frameHeight / 2 );
+		
+		frame.setResizable(false);
+		frame.setBounds( xp , yp , frameWidth  , frameHeight );
+		frame.setBackground(Color.gray);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	
+	
+
+	}
+
+}
